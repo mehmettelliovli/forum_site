43fd0dc2a3e8e928a9e20d7f7d6f5c47d5fef26c
Mehmet Emin Tellioğlu <mehmetemintellioglu@Mehmet-Mac.local>
2025-07-02 16:54:51 +0300

    history deleted

--- .history/backend/config_20250702123120.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://neondb_owner:npg_21ezrOHSnlLh@ep-solitary-butterfly-a8j9co8x-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&options=endpoint%3Dep-solitary-butterfly-a8j9co8x')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
